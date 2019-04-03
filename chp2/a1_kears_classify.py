3249ee8db42cd2c214377198e185b654d730a9dc
Freshield <zxdsw199182@gmail.com>
2019-04-03 11:59:48 +0800

    done the first test

---
+++ chp2/a1_kears_classify.py
@@ -0,0 +1,103 @@
+#coding=utf-8
+"""
+@Author: Freshield
+@License: (C) Copyright 2018, BEIJING LINKING MEDICAL TECHNOLOGY CO., LTD.
+@Contact: yangyufresh@163.com
+@File: a1_kears_classify.py
+@Time: 2019-07-01 10:47
+@Last_update: 2019-07-01 10:47
+@Desc: None
+@==============================================@
+@      _____             _   _     _   _       @
+@     |   __|___ ___ ___| |_|_|___| |_| |      @
+@     |   __|  _| -_|_ -|   | | -_| | . |      @
+@     |__|  |_| |___|___|_|_|_|___|_|___|      @
+@                                    Freshield @
+@==============================================@
+"""
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import sklearn
+import pandas as pd
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+import sys
+import time
+import tensorflow as tf
+from tensorflow.python import keras
+
+print(tf.__version__)
+print(sys.version_info)
+for module in mpl, np, pd, sklearn, tf, keras:
+    print(module.__name__, module.__version__)
+
+# get dataset
+fashion_mnist = keras.datasets.fashion_mnist
+(x_train_all, y_train_all), (x_test, y_test) = fashion_mnist.load_data()
+x_valid, x_train = x_train_all[:5000], x_train_all[5000:]
+y_valid, y_train = y_train_all[:5000], y_train_all[5000:]
+
+print(x_valid.shape, y_valid.shape)
+print(x_train.shape, y_train.shape)
+print(x_test.shape, y_test.shape)
+
+#show pic
+def show_single_iamge(img_arr):
+    plt.imshow(img_arr, cmap='binary')
+    plt.show()
+
+
+def show_imgs(n_rows, n_cols, x_data, y_data, class_names):
+    assert len(x_data) == len(y_data)
+    assert n_rows * n_cols < len(x_data)
+
+    plt.figure(figsize=(n_cols * 1.4, n_rows * 1.6))
+    for row in range(n_rows):
+        for col in range(n_cols):
+            index = n_cols * row + col
+            plt.subplot(n_rows, n_cols, index+1)
+            plt.imshow(x_data[index], cmap='binary',
+                       interpolation = 'nearest')
+            plt.axis('off')
+            plt.title(class_names[y_data[index]])
+
+    plt.show()
+
+
+class_names = ['T-shirt', 'Trouser', 'Pullover', 'Dress',
+               'Coat', 'Sandal', 'Shirt', 'Sneaker',
+               'Bag', 'Ankle boot']
+#
+# show_imgs(3, 5, x_train, y_train, class_names)
+
+# begin to use keras sequential model
+model = keras.models.Sequential([
+    keras.layers.Flatten(input_shape=[28, 28]),
+    keras.layers.Dense(300, activation='relu'),
+    keras.layers.Dense(100, activation='relu'),
+    keras.layers.Dense(10, activation='softmax')
+])
+
+model.compile(loss='sparse_categorical_crossentropy',
+              optimizer = 'sgd',
+              metrics = ['accuracy'])
+
+print(model.layers)
+print(model.summary())
+
+history = model.fit(x_train, y_train, epochs=10,
+                    validation_data=(x_valid, y_valid))
+
+print(type(history))
+print(history.history)
+
+def plot_learning_curves(history):
+    pd.DataFrame(history.history).plot(figsize=(8, 5))
+    plt.grid(True)
+    plt.gca().set_ylim(0, 1)
+    plt.show()
+
+plot_learning_curves(history)
+
+model.evaluate(x_test, y_test)
