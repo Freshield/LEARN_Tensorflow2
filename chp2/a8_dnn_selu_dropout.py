bbee9d2705779090412dc87792f8b35fd4414d06
Freshield <zxdsw199182@gmail.com>
2019-04-13 15:44:02 +0800

    done dnn selu dropout

---
+++ chp2/a8_dnn_selu_dropout.py
@@ -0,0 +1,90 @@
+#coding=utf-8
+"""
+@Author: Freshield
+@License: (C) Copyright 2018, BEIJING LINKING MEDICAL TECHNOLOGY CO., LTD.
+@Contact: yangyufresh@163.com
+@File: a8_dnn_selu_dropout.py
+@Time: 2019-04-12 15:40
+@Last_update: 2019-04-12 15:40
+@Desc: None
+@==============================================@
+@      _____             _   _     _   _       @
+@     |   __|___ ___ ___| |_|_|___| |_| |      @
+@     |   __|  _| -_|_ -|   | | -_| | . |      @
+@     |__|  |_| |___|___|_|_|_|___|_|___|      @
+@                                    Freshield @
+@==============================================@
+"""
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import sklearn
+import pandas as pd
+import os
+import sys
+import time
+import tensorflow as tf
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+from tensorflow.python import keras
+
+print(tf.__version__)
+print(sys.version_info)
+for module in mpl, np, pd, sklearn, tf, keras:
+    print(module.__name__, module.__version__)
+
+fashion_mnist = keras.datasets.fashion_mnist
+(x_train_all, y_train_all), (x_test, y_test) = fashion_mnist.load_data()
+x_valid, x_train = x_train_all[:5000], x_train_all[5000:]
+y_valid, y_train = y_train_all[:5000], y_train_all[5000:]
+
+print(x_valid.shape, y_valid.shape)
+print(x_train.shape, y_train.shape)
+print(x_test.shape, y_test.shape)
+
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+x_train_scaled = scaler.fit_transform(
+    x_train.astype(np.float32).reshape(-1, 1)).reshape(-1, 28, 28)
+x_valid_scaled = scaler.transform(
+    x_valid.astype(np.float32).reshape(-1, 1)).reshape(-1, 28, 28)
+x_test_scaled = scaler.transform(
+    x_test.astype(np.float32).reshape(-1, 1)).reshape(-1, 28, 28)
+
+model = keras.models.Sequential()
+model.add(keras.layers.Flatten(input_shape=x_train_scaled.shape[1:]))
+for i in range(20):
+    model.add(keras.layers.Dense(100, activation='selu'))
+model.add(keras.layers.AlphaDropout(rate=0.5))
+model.add(keras.layers.Dense(10, activation='softmax'))
+
+model.compile(loss='sparse_categorical_crossentropy',
+              optimizer='sgd',
+              metrics=['accuracy'])
+print(model.summary())
+
+logdir = 'data/dnn-callbacks'
+if not os.path.exists(logdir):
+    os.mkdir(logdir)
+output_model_file = os.path.join(logdir, 'fashion_minst_model.h5')
+
+callbacks = [
+    keras.callbacks.TensorBoard(logdir),
+    keras.callbacks.ModelCheckpoint(output_model_file,
+                                    save_best_only=True),
+    keras.callbacks.EarlyStopping(patience=5, min_delta=1e-3)
+]
+
+history = model.fit(x_train_scaled, y_train, epochs=10,
+                    validation_data=(x_valid_scaled, y_valid),
+                    callbacks=callbacks)
+
+def plot_learning_curves(history):
+    pd.DataFrame(history.history).plot(figsize=(8, 5))
+    plt.grid(True)
+    plt.gca().set_ylim(0, 3)
+    plt.show()
+
+plot_learning_curves(history)
+
+model.evaluate(x_test_scaled, y_test)
