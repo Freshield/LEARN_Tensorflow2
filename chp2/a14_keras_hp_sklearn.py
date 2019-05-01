bfb3ea7d60ecf2427b09a7e8f525a199dfcd33d5
Freshield <zxdsw199182@gmail.com>
2019-05-01 17:12:25 +0800

    done chp2

--- chp2/a14_keras_hp_sklearn.py
+++ chp2/a14_keras_hp_sklearn.py
@@ -52,28 +52,32 @@ x_train_scaled = scaler.fit_transform(x_train)
 x_valid_scaled = scaler.transform(x_valid)
 x_test_scaled = scaler.transform(x_test)
 
+def build_model(hidden_layers=1,
+                layer_size=30,
+                learning_rate=3e-3):
+    model = keras.models.Sequential()
+    model.add(keras.layers.Dense(layer_size, activation='relu',
+                                 input_shape=x_train_scaled.shape[1:]))
 
-learning_rates = [1e-4, 3e-4, 1e-3, 3e-3, 1e-2, 3e-2]
-histories = []
-for lr in learning_rates:
-    model = keras.models.Sequential([
-        keras.layers.Dense(30, activation='relu',
-                           input_shape=x_train.shape[1:]),
-        keras.layers.Dense(1)
-    ])
-    optimizer = keras.optimizers.SGD(lr=lr)
+    for i in range(hidden_layers - 1):
+        model.add(keras.layers.Dense(layer_size,
+                                     activation='relu'))
 
+    model.add(keras.layers.Dense(1))
+    optimizer = keras.optimizers.SGD(lr=learning_rate)
     model.compile(loss='mse', optimizer=optimizer)
-    callbacks = [keras.callbacks.EarlyStopping(
-        patience=5, min_delta=1e-2
-    )]
+    return model
 
-    history = model.fit(x_train_scaled, y_train,
-                        validation_data=(x_valid_scaled, y_valid),
-                        epochs=20,
-                        callbacks=callbacks)
+sklearn_model = keras.wrappers.scikit_learn.KerasRegressor(
+    build_fn=build_model
+)
+callbacks = [keras.callbacks.EarlyStopping(patience=5, min_delta=1e-2)]
+# history = sklearn_model.fit(x_train_scaled, y_train,
+#                             epochs = 10,
+#                             validation_data=(x_valid_scaled, y_valid),
+#                             callbacks=callbacks)
 
-    histories.append(history)
+
 
 def plot_learning_curves(history):
     pd.DataFrame(history.history).plot(figsize=(8, 5))
@@ -81,6 +85,29 @@ def plot_learning_curves(history):
     plt.gca().set_ylim(0, 1)
     plt.show()
 
-for lr, history in zip(learning_rates, histories):
-    print('Learning rat: ', lr)
-    plot_learning_curves(history)
+# plot_learning_curves(history)
+
+from scipy.stats import reciprocal
+
+param_distribution = {
+    "hidden_layers":[1, 2, 3, 4],
+    "layer_size": np.arange(1, 100),
+    "learning_rate": reciprocal(1e-4, 1e-2),
+}
+
+from sklearn.model_selection import RandomizedSearchCV
+
+random_search_cv = RandomizedSearchCV(
+    sklearn_model, param_distribution,
+    n_iter=10, cv=3, n_jobs=1
+)
+
+random_search_cv.fit(x_train_scaled, y_train,
+                     epochs=10, validation_data=(x_valid_scaled, y_valid),
+                     callbacks=callbacks)
+
+print(random_search_cv.best_params_)
+print(random_search_cv.best_score_)
+
+model = random_search_cv.best_estimator_.model
+model.evaluate(x_test_scaled, y_test)
