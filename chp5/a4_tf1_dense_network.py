3cd2528b92dfaa94508b3107d750b5ee3bf0fe4b
Freshield <zxdsw199182@gmail.com>
2019-06-07 16:01:34 +0800

    interrupt

---
+++ chp5/a4_tf1_dense_network.py
@@ -0,0 +1,33 @@
+#coding=utf-8
+"""
+@Author: Freshield
+@License: (C) Copyright 2018, BEIJING LINKING MEDICAL TECHNOLOGY CO., LTD.
+@Contact: yangyufresh@163.com
+@File: a4_tf1_dense_network.py
+@Time: 2019-07-15 15:59
+@Last_update: 2019-07-15 15:59
+@Desc: None
+@==============================================@
+@      _____             _   _     _   _       @
+@     |   __|___ ___ ___| |_|_|___| |_| |      @
+@     |   __|  _| -_|_ -|   | | -_| | . |      @
+@     |__|  |_| |___|___|_|_|_|___|_|___|      @
+@                                    Freshield @
+@==============================================@
+"""
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import sklearn
+import pandas as pd
+import os
+import sys
+import time
+import tensorflow as tf
+
+from tensorflow._api.v1 import keras
+
+print(tf.__version__)
+print(sys.version_info)
+for module in mpl, np, pd, sklearn, tf, keras:
+    print(module.__name__, module.__version__)
