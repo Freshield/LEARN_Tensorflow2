100cfcf1839c7576d7969050b07cf7e57941ba13
Freshield <zxdsw199182@gmail.com>
2019-07-07 11:10:53 +0800

    done the bidriection rnn

---
+++ chp7/a3_bidirection_rnn.py
@@ -0,0 +1,120 @@
+#coding=utf-8
+"""
+@Author: Freshield
+@License: (C) Copyright 2018, BEIJING LINKING MEDICAL TECHNOLOGY CO., LTD.
+@Contact: yangyufresh@163.com
+@File: a3_bidirection_rnn.py
+@Time: 2019-07-07 11:05
+@Last_update: 2019-07-07 11:05
+@Desc: None
+@==============================================@
+@      _____             _   _     _   _       @
+@     |   __|___ ___ ___| |_|_|___| |_| |      @
+@     |   __|  _| -_|_ -|   | | -_| | . |      @
+@     |__|  |_| |___|___|_|_|_|___|_|___|      @
+@                                    Freshield @
+@==============================================@
+"""
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import sklearn
+import pandas as pd
+import os
+import sys
+import time
+import tensorflow as tf
+
+from tensorflow.python.keras.api._v2 import keras
+
+print(tf.__version__)
+print(sys.version_info)
+for module in mpl, np, pd, sklearn, tf, keras:
+    print(module.__name__, module.__version__)
+
+
+imdb = keras.datasets.imdb
+vocab_size = 10000
+index_from = 3
+(train_data, train_labels), (test_data, test_labels) = imdb.load_data(
+    path='imdb.npz', num_words=vocab_size, index_from=index_from)
+
+train_data = train_data[:5000]
+train_labels = train_labels[:5000]
+
+word_index = imdb.get_word_index()
+print(word_index)
+print(len(word_index))
+
+# 是word: id的字典
+word_index = {k:(v+3) for k, v in word_index.items()}
+
+word_index['<PAD>'] = 0
+word_index['<START>'] = 1
+word_index['<UNK>'] = 2
+word_index['<END>'] = 3
+
+# 是id: word的字典
+reverse_word_index = {v: k for k, v in word_index.items()}
+
+def decode_view(text_ids):
+    return ' '.join(
+        [reverse_word_index.get(word_id, '<UNK>') for word_id in text_ids]
+    )
+
+print(decode_view(train_data[0]))
+
+max_length = 500
+
+train_data = keras.preprocessing.sequence.pad_sequences(
+    train_data,
+    value=word_index['<PAD>'],
+    padding='post',
+    maxlen=max_length
+)
+
+test_data = keras.preprocessing.sequence.pad_sequences(
+    test_data,
+    value=word_index['<PAD>'],
+    padding='post',
+    maxlen=max_length
+)
+
+print(train_data[0])
+
+embedding_dim = 16
+batch_size = 128
+
+single_rnn_model = keras.models.Sequential([
+    keras.layers.Embedding(vocab_size, embedding_dim, input_length=max_length),
+    keras.layers.Bidirectional(
+        keras.layers.SimpleRNN(units=64, return_sequences=True)
+    ),
+    keras.layers.Bidirectional(
+        keras.layers.SimpleRNN(units=64, return_sequences=False)
+    ),
+    keras.layers.Dense(64, activation='relu'),
+    keras.layers.Dense(1, activation='sigmoid')
+])
+
+single_rnn_model.summary()
+single_rnn_model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+history = single_rnn_model.fit(
+    train_data, train_labels,
+    epochs=3, batch_size=batch_size, validation_split=0.2)
+
+
+def plot_learning_curves(history, label, epochs, min_value, max_value):
+    data = {}
+    data[label] = history.history[label]
+    data['val_'+label] = history.history['val_'+label]
+    pd.DataFrame(data).plot(figsize=(8, 5))
+    plt.grid(True)
+    plt.axis([0, epochs, min_value, max_value])
+    plt.show()
+
+plot_learning_curves(history, 'accuracy', 3, 0, 1)
+plot_learning_curves(history, 'loss', 3, 0, 1)
+
