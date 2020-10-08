98cd7f0b0972003e5c1909dc7996dd2492603cae
freshyu <freshyu@tencent.com>
2020-10-08 09:54:17 +0800

    begin the time series part

---
+++ official_tutorial/a09_pix_to_pix.py
@@ -0,0 +1,27 @@
+# coding=utf-8
+"""
+@Author: Freshield
+@Contact: yangyufresh@163.com
+@File: a9_pix_to_pix.py
+@Time: 2020-10-07 18:22
+@Last_update: 2020-10-07 18:22
+@Desc: None
+@==============================================@
+@      _____             _   _     _   _       @
+@     |   __|___ ___ ___| |_|_|___| |_| |      @
+@     |   __|  _| -_|_ -|   | | -_| | . |      @
+@     |__|  |_| |___|___|_|_|_|___|_|___|      @
+@                                    Freshield @
+@==============================================@
+"""
+import os
+import tensorflow as tf
+
+
+_URL = 'https://people.eecs.berkeley.edu/~tinghuiz/projects/pix2pix/datasets/facades.tar.gz'
+
+path_to_zip = tf.keras.utils.get_file('facades.tar.gz',
+                                      origin=_URL,
+                                      extract=True)
+
+PATH = os.path.join(os.path.dirname(path_to_zip), 'facades/')
