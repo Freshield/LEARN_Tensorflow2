7611bf6955ed457f317fa93a44b17c696f2d433b
freshyu <freshyu@tencent.com>
2020-10-08 15:43:35 +0800

    continue

--- official_tutorial/a10_time_series.py
+++ official_tutorial/a10_time_series.py
@@ -14,3 +14,233 @@
 @                                    Freshield @
 @==============================================@
 """
+import os
+import datetime
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import tensorflow as tf
+
+mpl.rcParams['figure.figsize'] = (8, 6)
+mpl.rcParams['axes.grid'] = False
+
+zip_path = tf.keras.utils.get_file(
+    origin='https://storage.googleapis.com/tensorflow/tf-keras-datasets/jena_climate_2009_2016.csv.zip',
+    fname='jena_climate_2009_2016.csv.zip',
+    extract=True)
+csv_path, _ = os.path.splitext(zip_path)
+print(csv_path)
+
+df = pd.read_csv(csv_path)
+df = df[5::6]
+
+date_time = pd.to_datetime(df.pop('Date Time'), format='%d.%m.%Y %H:%M:%S')
+print(df.head())
+print(date_time.head())
+
+# plot_cols = ['T (degC)', 'p (mbar)', 'rho (g/m**3)']
+# plot_features = df[plot_cols]
+# plot_features.index = date_time
+# _ = plot_features.plot(subplots=True)
+# plt.show()
+# plot_features = df[plot_cols][:480]
+# plot_features.index = date_time[:480]
+# _ = plot_features.plot(subplots=True)
+# plt.show()
+
+print(df.describe().transpose())
+
+wv = df['wv (m/s)']
+bad_wv = wv == -9999.0
+wv[bad_wv] = 0.0
+
+max_wv = df['max. wv (m/s)']
+bad_max_wv = max_wv == -9999.0
+max_wv[bad_max_wv] = 0.0
+
+print(df['wv (m/s)'].min())
+
+# plt.hist2d(df['wd (deg)'], df['wv (m/s)'], bins=(50, 50), vmax=400)
+# plt.colorbar()
+# plt.xlabel('Wind Direction [deg]')
+# plt.ylabel('Wind Velocity [m/s]')
+# plt.show()
+
+wv = df.pop('wv (m/s)')
+max_wv = df.pop('max. wv (m/s)')
+
+wd_rad = df.pop('wd (deg)') * np.pi / 180
+
+df['Wx'] = wv * np.cos(wd_rad)
+df['Wy'] = wv * np.sin(wd_rad)
+
+df['max Wx'] = max_wv * np.cos(wd_rad)
+df['max Wy'] = max_wv * np.sin(wd_rad)
+
+# plt.hist2d(df['Wx'], df['Wy'], bins=(50, 50), vmax=400)
+# plt.colorbar()
+# plt.xlabel('Wind X [m/s]')
+# plt.ylabel('Wind Y [m/x]')
+# ax = plt.gca()
+# ax.axis('tight')
+# plt.show()
+
+timestamp_s = date_time.map(datetime.datetime.timestamp)
+
+day = 24 * 60 * 60
+year = (365.2425) * day
+
+df['Day sin'] = np.sin(timestamp_s * (2 * np.pi / day))
+df['Day cos'] = np.cos(timestamp_s * (2 * np.pi / day))
+df['Year sin'] = np.sin(timestamp_s * (2 * np.pi / year))
+df['Year cos'] = np.cos(timestamp_s * (2 * np.pi / year))
+
+# plt.plot(np.array(df['Day sin'])[:25])
+# plt.plot(np.array(df['Day cos'])[:25])
+# plt.xlabel('Time [h]')
+# plt.title('Time of day signal')
+# plt.show()
+
+column_indices = {name: i for i, name in enumerate(df.columns)}
+
+n = len(df)
+train_df = df[0:int(n*0.7)]
+val_df = df[int(n*0.7): int(n*0.9)]
+test_df = df[int(n*0.9):]
+
+num_features = df.shape[1]
+print(num_features)
+
+train_mean = train_df.mean()
+train_std = train_df.std()
+
+train_df = (train_df - train_mean) / train_std
+val_df = (val_df - train_mean) / train_std
+test_df = (test_df - train_mean) / train_std
+
+df_std = (df - train_mean) / train_std
+df_std = df_std.melt(var_name='Column', value_name='Normalized')
+
+# plt.figure(figsize=(12, 6))
+# ax = sns.violinplot(x='Column', y='Normalized', data=df_std)
+# _ = ax.set_xticklabels(df.keys(), rotation=90)
+# plt.show()
+
+
+class WindowGenerator():
+    def __init__(
+            self, input_width, label_width, shift,
+            train_df=train_df, val_df=val_df, test_df=test_df,
+            label_columns=None):
+        self.train_df = train_df
+        self.val_df = val_df
+        self.test_df = test_df
+
+        self.label_columns = label_columns
+        if label_columns is not None:
+            self.label_columns_indices = {name: i for i, name in enumerate(label_columns)}
+        self.column_indeices = {name: i for i, name in enumerate(train_df.columns)}
+
+        self.input_width = input_width
+        self.label_width = label_width
+
+        self.total_window_size = input_width + shift
+
+        self.input_slice = slice(0, input_width)
+        self.input_indices = np.arange(self.total_window_size)[self.input_slice]
+
+        self.label_start = self.total_window_size - self.label_width
+        self.labels_slice = slice(self.label_start, None)
+        self.label_indices = np.arange(self.total_window_size)[self.labels_slice]
+
+    def __repr__(self):
+        return '\n'.join([
+            f'Total window size: {self.total_window_size}',
+            f'Input indices: {self.input_indices}',
+            f'Label indices: {self.label_indices}',
+            f'Label column name(s): {self.label_columns}'
+        ])
+
+    def split_window(self, features):
+        inputs = features[:, self.input_slice, :]
+        labels = features[:, self.labels_slice, :]
+        if self.label_columns is not None:
+            labels = tf.stack(
+                [labels[:, :, self.column_indeices[name]] for name in self.label_columns], axis=-1)
+
+        inputs.set_shape([None, self.input_width, None])
+        labels.set_shape([None, self.label_width, None])
+
+        return inputs, labels
+
+    def plot(self, model=None, plot_col='T (degC)', max_subplots=3):
+        inputs, labels = self.example
+        plt.figure(figsize=(12, 8))
+        plot_col_index = self.column_indeices[plot_col]
+        max_n = min(max_subplots, len(inputs))
+        for n in range(max_n):
+            plt.subplot(3, 1, n + 1)
+            plt.ylabel(f'{plot_col} [normed]')
+            plt.plot(self.input_indices, inputs[n, :, plot_col_index],
+                     label='Inputs', marker='.', zorder=-10)
+
+            if self.label_columns:
+                label_col_index = self.label_columns_indices.get(plot_col, None)
+            else:
+                label_col_index = plot_col_index
+
+            if label_col_index is None:
+                continue
+
+            plt.scatter(self.label_indices, labels[n, :, label_col_index],
+                        edgecolors='k', label='Labels', c='#2ca02c', s=64)
+            if model is not None:
+                predictions = model(inputs)
+                plt.scatter(self.label_indices, predictions[n, :, label_col_index],
+                            marker='X', edgecolors='k', label='Predictions',
+                            c='#ff7f0e', s=64)
+
+            if n == 0:
+                plt.legend()
+
+        plt.xlabel('Time [h]')
+        plt.show()
+
+    def make_dataset(self, data):
+        data = np.array(data, dtype=np.float32)
+        ds = tf.keras.preprocessing.timeseries_dataset_from_array(
+            data=data, targets=None, sequence_length=self.total_window_size,
+            sequence_stride=1, shuffle=True, batch_size=32
+        )
+
+        ds = ds.map(self.split_window)
+
+        return ds
+
+    @property
+    def train(self):
+        return self.make_dataset(self.train_df)
+
+
+w1 = WindowGenerator(input_width=24, label_width=1, shift=24, label_columns=['T (degC)'])
+print(w1)
+w2 = WindowGenerator(input_width=6, label_width=1, shift=1, label_columns=['T (degC)'])
+print(w2)
+
+example_windows = tf.stack([np.array(train_df[: w2.total_window_size]),
+                            np.array(train_df[100:100+w2.total_window_size]),
+                            np.array(train_df[200:200+w2.total_window_size])])
+
+example_inputs, example_labels = w2.split_window(example_windows)
+print('All shapes are: (batch, time, features)')
+print(f'Windows shape: {example_windows.shape}')
+print(f'Inputs shape: {example_inputs.shape}')
+print(f'labels shape: {example_labels.shape}')
+
+w2.example = example_inputs, example_labels
+w2.plot()
+
+
---
+++ official_tutorial/test/t2_test_slice.py
@@ -0,0 +1,20 @@
+# coding=utf-8
+"""
+@Author: Freshield
+@Contact: yangyufresh@163.com
+@File: t2_test_slice.py
+@Time: 2020-10-08 12:12
+@Last_update: 2020-10-08 12:12
+@Desc: None
+@==============================================@
+@      _____             _   _     _   _       @
+@     |   __|___ ___ ___| |_|_|___| |_| |      @
+@     |   __|  _| -_|_ -|   | | -_| | . |      @
+@     |__|  |_| |___|___|_|_|_|___|_|___|      @
+@                                    Freshield @
+@==============================================@
+"""
+
+input_slice = slice(0, 10)
+input_indices = list(range(110))[input_slice]
+print(input_indices)
